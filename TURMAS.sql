bfd0fa95ae1b8d0b96d75d515026f34fd8907d19
Yasmin Gomes Fabiano. <yasmingfabian@gmail.com>
2025-09-03 16:36:47 -0300

    Update TURMAS.sql

--- TURMAS.sql
+++ TURMAS.sql
@@ -51,4 +51,43 @@ CREATE TABLE Historico(
 	FOREIGN KEY (codigo_professor_id) REFERENCES Professores(id),
     FOREIGN KEY (codigo_turma_id) REFERENCES Turma(id)
 );
+INSERT INTO Alunos(matricula, nome, endereco, cidade) VALUES
+('2015010101', 'José de Alencar','Rua das Almas', 'Natal'),
+('2015010102', 'João José','Avenida Ruy Carneiro', 'João Pessoa'),
+('2015010103', 'Maria Joaquina','Rua das Carrossel', 'Recife'),
+('2015010104', 'Maria das Dores','Rua das Ladeiras', 'Fortaleza'),
+('2015010105', 'José Claudino dos Santos','Centro', 'Natal'),
+('2015010106', 'Josuélisson Claudino dos santos','Centro', 'Natal');
+
+
+INSERT INTO Disciplinas(codigo_disciplina, nome_disciplina, carga_horaria) VALUES
+('BD', 'Banco de Dados',100),
+('POO', 'Programação com acesso de banco de dados', 100),
+('WEB', 'Autoria Web', 50),
+('ENG', 'Engenharia de sofware', 80);
+
+INSERT INTO Professores(codigo_professor, nome, endereco, cidade) VALUES
+('212131', 'Nickerson Ferreira', 'Rua Manaíba', 'João Pessoa'),
+('212135', 'Adorilson Bezerra', 'Avenida Salgado Filho', 'Natal'),
+('192011', 'Diego Oliveira', 'Avenida Roberto Freire', 'Natal');
+
+INSERT INTO Turma(codigo_disciplina_id, codigo_turma, codigo_professor_id, ano, horario)
+VALUES
+('BD', 1, '212131', 2015, '11H-12H'),
+('BD', 2, '212131', 2015, '13H-14H'),
+('POO', 1, '192011', 2015, '08H-09H'),
+('WEB', 1, '192011', 2015, '07H-08H'),
+('ENG', 1, '122135', 2015, '10H-11H');
+
+
+/*a) Encontre a MAT dos alunos com nota em BD em 2015 
+menor que 5 (obs: BD =
+código da disciplinas).*/
+SELECT
+
+
+/*Encontre quantos alunos não são de Natal.*/
+SELECT*FROM Alunos WHERE cidade != 'Natal';
+
+
 
