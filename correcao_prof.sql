1bc64ec57cd69b51efba7e254b2cbd89ddffabac
Yasmin Gomes Fabiano. <yasmingfabian@gmail.com>
2025-09-05 16:31:56 -0300

    Update correcao_prof.sql

--- correcao_prof.sql
+++ correcao_prof.sql
@@ -119,6 +119,71 @@ INSERT INTO Historico (mat, cod_disc, cod_turma, cod_prof, ano, frequencia, nota
 
 SELECT*FROM Historico;
 
+SELECT * FROM Historico;
+SELECT DISTINCT MAT 
+FROM Historico
+WHERE cod_disc = 'BD' AND ano=2015 AND nota <5;
+
+
+SELECT MAT, AVG(nota) AS media_nota
+FROM Historico
+WHERE cod_disc='POO' AND ano=2015
+GROUP BY MAT;
+
+
+SELECT MAT, AVG(nota) AS media_nota
+FROM Historico
+WHERE cod_disc='POO' AND ano=2015
+GROUP BY MAT
+HAVING AVG(nota) >6;
+
+SELECT COUNT(*) AS quantidade_alunos_fora_natal
+FROM alunos
+WHERE CIDADE <> 'NATAL';
+
+
+
+/* Liste os nomes dos alunos que tiraram nota maior que 7 na disciplina "ENG" 
+em 2015.*/
+SELECT nome
+FROM Alunos
+WHERE mat IN(
+	SELECT mat
+	FROM Historico
+	WHERE cod_disc='ENG' AND ano=2015 AND nota >=7
+);
+
+
+
+
+/*Mostre para cada professor o nome, cidade, e a quantidade de turmas que 
+ele ministrou em 2015*/
+
+SELECT nome, cidade, COUNT(*) AS quantidade_turma
+FROM Professores 
+JOIN Turma  ON professores.cod_prof= turma.cod_prof
+WHERE turma.ano=2015
+GROUP BY professores.nome, professores.cidade;
+
+
+/*Liste o nome dos alunos, o nome da disciplina e a nota obtida, ordenado 
+pelo nome do aluno e pela disciplina, para o ano de 2015.*/
+SELECT nome AS aluno,
+       nome_dis AS disciplina,
+       nota
+FROM Alunos 
+JOIN Historico ON mat = mat
+JOIN Disciplinas ON cod_disc = cod_disc
+WHERE ano = 2015
+ORDER BY nome, nome_dis; -- incompleto
+
+/*Encontre o aluno com a maior média geral de notas em todas as disciplinas 
+em 2015. Mostre o MAT, nome e média de notas.*/
+SELECT mat, AVG(nota) AS media
+FROM Historico 
+WHERE ano=2015
+GROUP BY mat;
+
 
 
                                                                                                                                                                                                                                                                                                                                
